3ea24d02832c31b8a6d94315d07eae92121cca06
Gabriel Tonete <gabtopi77@gmail.com>
2022-02-22 11:50:44 -0300

    cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "http://reactjsgabt.herokuapp.com, http://localhost:3000");
     res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "*");
     next();
 };
+
